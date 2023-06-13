65834352c35bd3a0f39e455e42f2445eaeac1315
Sanskar Kakde <sanskarkcmc20@svvv.edu.in>
2023-06-14 03:36:39 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sanskar Kakde",
+  title: "Backend Web Developer",
+  email: "sanskar.kakde@studentambassadors.com",
+  gitHub: "sanskarkakde14",
+  instagram: "Sanskarr.14",
+  linkedIn: "sanskar kakde",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "sanskar_kakde",
+  youTube: "sanskar kakde",
 };
 
 const primaryColor = "#4E567E";
