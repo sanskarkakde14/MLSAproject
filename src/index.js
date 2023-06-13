45f646e2e7ad1bdea21da6f9bffb2cec7555d90b
Sanskar Kakde <sanskarkcmc20@svvv.edu.in>
2023-06-14 03:25:34 +0530

    f

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { render } from "react-dom";
-
+import ReactDom from "react-dom";
 import App from "./App";
 
-render(<App></App>, document.getElementById("app"))
+ReactDom.render(<App></App>, document.getElementById("app"));
